82ebd375589c9d6febc8788fcf80ea6948cae07d
susantapaul01 <boxme.sp@gmail.com>
2024-01-05 16:33:21 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+function App() {
+  return (
+    <>
+      <div className='flex h-screen'>
+        <h1 className='m-auto text-3xl font-bold text-green-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae tempora hic commodi similique autem fugiat?</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
